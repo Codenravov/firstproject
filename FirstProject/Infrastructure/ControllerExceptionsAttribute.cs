8aa17a9567c9d9b38218ec7471d7281ba0222f14
n.grigoruk <n.grigoruk@itransition.com>
2018-08-13 18:48:11 +0300

    Add Controller Exeption Attribute

---
+++ FirstProject/Infrastructure/ControllerExceptionsAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebProject.Infrastructure
+{
+    public class ControllerExceptionsAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            string actionName = context.RouteData.Values["action"].ToString();
+            string exceptionStack = context.Exception.StackTrace;
+            string exceptionMessage = context.Exception.Message;
+            context.Result = new ContentResult
+            {
+                Content = $"В методе {actionName} возникло исключение: \n {exceptionMessage} \n {exceptionStack}"
+            };
+            context.ExceptionHandled = true;
+        }
+    }
+}
